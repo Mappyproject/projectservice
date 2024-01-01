c435feb6c6c5290cf8240bab3e69ce1c454e27ad
Iliaselh <ilias@primeitsolutions.nl>
2024-01-01 17:26:42 +0100

    removed unneccessary comments

--- src/main/java/com/mappy/projectservice/ProjectserviceApplication.java
+++ src/main/java/com/mappy/projectservice/ProjectserviceApplication.java
@@ -2,11 +2,12 @@ package com.mappy.projectservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProjectserviceApplication {
 	public static void main(String[] args) {
-		System.setProperty("application.properties", "project-service");
 		SpringApplication.run(ProjectserviceApplication.class, args);
 	}
 
